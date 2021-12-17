c19a87c2fe5f52c3ee0e4ecf9642efc9f54d301e
Shubham Agarwal <shubham.27052@gmail.com>
2021-12-17 22:31:57 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useEffect } from "react";
+import ReactDom from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Placard from "./placard";
+import "./index.css";
+import reviews from "./data";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Main = () => {
+  const [value, setValue] = useState(0);
+  const changePos = () => {
+    console.log(value);
+    if (value > 2) {
+      setValue(0);
+    } else {
+      setValue(value + 1);
+    }
+  };
+
+  const changeNeg = () => {
+    console.log(value);
+    if (value < 1) {
+      setValue(3);
+    } else {
+      setValue(value - 1);
+    }
+  };
+
+  const suprise = () => {
+    let x = Math.floor(Math.random() * (3 - 0 + 1) + 0);
+    setValue(x);
+  };
+
+  return (
+    <>
+      <h1 className="main-heading">Our Team</h1>
+      <Placard
+        {...reviews[value]}
+        changePos={changePos}
+        changeNeg={changeNeg}
+        suprise={suprise}
+      />
+    </>
+  );
+};
+
+ReactDom.render(<Main />, document.getElementById("root"));
